4819d22300291244755ba4073435f1387a6b216d
Petr Sima <ptea.sima@gmail.com>
2019-06-09 02:27:41 +0200

    Initial Commit

---
+++ Counters/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Counters
+//
+//  Created by Petr Šíma on 09/06/2019.
+//  Copyright © 2019 Petr Šíma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif

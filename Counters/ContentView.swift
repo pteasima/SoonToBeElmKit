edf056843ee41b1a6a26aeea143130bde3f5fa2b
Petr Sima <ptea.sima@gmail.com>
2019-06-20 01:05:25 +0200

    passing animation transactions on dispatch. I probably wont use this, and will instead impose animation top-down from the Store

--- Counters/ContentView.swift
+++ Counters/ContentView.swift
@@ -1,37 +1,60 @@
 import SwiftUI
 import Combine
 
-typealias Store<State, Action> = ObjectBinding<ViewStore<State>>
-final class ViewStore<State>: BindableObject {
+final class P {
+    let store: Store<ExampleApp.State, ExampleApp.Action> = ObjectBinding(initialValue: ExampleApp.theStore)
+    func dispatch(_ action: ExampleApp.Action, transaction: Transaction? = nil) {
+        print("jo \(action)")
+        let storeToState: ReferenceWritableKeyPath<ViewStore<ExampleApp.State, ExampleApp.Action>, ExampleApp.State> = \.state
+        self.store.delegateValue[dynamicMember: storeToState].transaction(transaction!).value = ExampleApp.State(isOn: false)
+//        self.store.delegateValue[dynamicMember: storeToState].animation().value = ExampleApp.State(isOn: false)
+    }
+}
+var p = P()
+
+typealias Store<State, Action> = ObjectBinding<ViewStore<State, Action>>
+
+final class ViewStore<State, Action>: BindableObject {
     let didChange = PassthroughSubject<(), Never>()
     init(state: State) {
         self.state = state
     }
     var state: State {
         didSet {
-            //this nesting ensures didChange is always triggered. And, with @dynamicMemberLookup we'll get rid of `.state` nesting everywhere
             didChange.send(())
         }
     }
+    func dispatch(_ action: Action, transaction: Transaction? = nil) {
+        p.dispatch(action as! ExampleApp.Action, transaction: transaction)
+    }
 }
 
-// this isnt very Elm-y (yet!), its just a View that has a single Store
 @dynamicMemberLookup protocol ElmView: View {
     associatedtype State
-    var store: ObjectBinding<ViewStore<State>> { get }
+    associatedtype Action
+    var store: ObjectBinding<ViewStore<State, Action>> { get }
     
-    subscript<Subject>(dynamicMember keyPath: WritableKeyPath<State, Subject>) -> Binding<Subject> { get }
+    subscript<Subject>(dynamicMember keyPath: WritableKeyPath<State, Subject>) -> (@escaping (Subject) -> Action) -> Binding<Subject> { get }
 }
 extension ElmView {
-    subscript<Subject>(dynamicMember keyPath: WritableKeyPath<State, Subject>) -> Binding<Subject> {
-        let storeToState: ReferenceWritableKeyPath<ViewStore<State>, State> = \.state
+    func dispatch(_ action: Action, transaction: Transaction? = nil) {
+        store.value.dispatch(action, transaction: transaction)
+    }
+    
+    subscript<Subject>(dynamicMember keyPath: WritableKeyPath<State, Subject>) -> (@escaping (Subject) -> Action) -> Binding<Subject> {
+        let storeToState: ReferenceWritableKeyPath<ViewStore<State, Action>, State> = \.state
         let storeToSubject = storeToState.appending(path: keyPath)
-        return Binding(getValue: {
+        return { transform in
+            Binding(getValue: {
             self.store.delegateValue[dynamicMember: storeToSubject].value
             }, setValue: { newValue, transaction in
+                self.dispatch(transform(newValue), transaction: transaction)
                 //without `transaction`, animated Bindings wouldnt animate. Using transaction like this seems to be the right way to "compose Bindings"
-                self.store.delegateValue[dynamicMember: storeToSubject].transaction(transaction).value = newValue
+//                self.store.delegateValue[dynamicMember: storeToSubject].transaction(transaction).value = newValue
+//                self.store.delegateValue[dynamicMember: storeToState].transaction(transaction).value = ExampleApp.State(isOn: newValue as! Bool) as! State
         })
+            
+        }
         
         
     }
@@ -41,16 +64,38 @@ enum ExampleApp {
     struct State {
         var isOn: Bool = true
     }
-    static let theStore = ViewStore(state: State())
+    enum Action {
+        case none
+    }
+    static let theStore = ViewStore<State, Action>(state: State())
 }
 
 struct ContentView : ElmView {
-    let store: Store<ExampleApp.State, Never>
+    let store: Store<ExampleApp.State, ExampleApp.Action>
     
     var body: some View {
-        List(0...100) { _ in
-            Toggle(isOn: self.isOn.animation()) {
-                Text("is it on?")
+        VStack {
+            V2(store: store)
+            V2(store: store)
+            List(0...100) { _ in
+                Toggle(isOn: self.isOn { _ in .none }.animation()) {
+                    Text("is it on?")
+                }
+            }
+        }
+    }
+}
+
+struct V2: ElmView {
+    let store: Store<ExampleApp.State, ExampleApp.Action>
+    
+    var body: some View {
+        VStack {
+            Toggle(isOn: self.isOn { _ in .none }.animation()) {
+                Text("v2 is it on?")
+            }
+            Toggle(isOn: self.isOn { _ in .none }.animation()) {
+                Text("v22 is it on?")
             }
         }
     }

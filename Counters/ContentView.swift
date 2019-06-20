17060c59b72eb5c0c1b54e5f26d7e4a1ce8f4309
Petr Sima <ptea.sima@gmail.com>
2019-06-20 10:50:08 +0200

    first actual dispatches to fake store

--- Counters/ContentView.swift
+++ Counters/ContentView.swift
@@ -5,10 +5,21 @@ final class P {
     let store: Store<ExampleApp.State, ExampleApp.Action> = ObjectBinding(initialValue: ExampleApp.theStore)
     func dispatch(_ action: ExampleApp.Action, transaction: Transaction? = nil) {
         print("jo \(action)")
+        print(transaction, transaction?.disablesAnimations, transaction?.animation, transaction?.isContinuous)
+ 
+        switch action {
+        case .none:
+            break
+        case .toggle(let newValue):
+            state.isOn = newValue
+        case .onDrag(let newX):
+            state.dragged = newX
+        }
+//        self.store.delegateValue[dynamicMember: storeToState].transaction(transaction!).value = state
         let storeToState: ReferenceWritableKeyPath<ViewStore<ExampleApp.State, ExampleApp.Action>, ExampleApp.State> = \.state
-        self.store.delegateValue[dynamicMember: storeToState].transaction(transaction!).value = ExampleApp.State(isOn: false)
-//        self.store.delegateValue[dynamicMember: storeToState].animation().value = ExampleApp.State(isOn: false)
+        self.store.delegateValue[dynamicMember: storeToState].animation().value = state
     }
+    var state: ExampleApp.State = .init()
 }
 var p = P()
 
@@ -63,16 +74,19 @@ extension ElmView {
 enum ExampleApp {
     struct State {
         var isOn: Bool = true
+        var dragged: Double = 0.0
     }
     enum Action {
         case none
+        case toggle(Bool)
+        case onDrag(Double)
     }
     static let theStore = ViewStore<State, Action>(state: State())
 }
 
 struct ContentView : ElmView {
     let store: Store<ExampleApp.State, ExampleApp.Action>
-    
+
     var body: some View {
         VStack {
             V2(store: store)
@@ -91,13 +105,21 @@ struct V2: ElmView {
     
     var body: some View {
         VStack {
-            Toggle(isOn: self.isOn { _ in .none }.animation()) {
+//            Slider(value: self.sliderValue { _ in .none })
+            Toggle(isOn: self.isOn { .toggle($0)  }.animation()) {
                 Text("v2 is it on?")
             }
-            Toggle(isOn: self.isOn { _ in .none }.animation()) {
+            Toggle(isOn: self.isOn { .toggle($0) }.animation()) {
                 Text("v22 is it on?")
             }
+//            Text("\(self.dragged.value)")
         }
+            .gesture(
+            DragGesture()
+                .onChanged { value in
+                    self.dispatch(.onDrag(Double(value.location.x)))
+                }
+        )
     }
 }
 
@@ -109,22 +131,3 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
-
-
-
-
-
-
-//final class Store<State, Action> {
-//    func dispatch(_ action: Action) {
-//        _ = output.receive(action)
-//    }
-//    let output: AnySubscriber<Action, Never>
-//    let cancellable =
-//    init(input: AnyPublisher<State, Never>, output: AnySubscriber<Action, Never>) {
-//        self.output = output
-//        input.assign(to: \.state, on: self)
-//    }
-//    var state: State!
-//}
-
